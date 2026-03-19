69c793f0bdc3cacdd57561204efccb7de86b4d4e
Shams Ashraf <227019490+shamsashraaf@users.noreply.github.com>
2026-03-19 04:03:20 +0200

    add controllers and make a php router

--- partials/nav.php
+++ partials/nav.php
@@ -9,8 +9,8 @@
                             <div class="ml-10 flex items-baseline space-x-4">
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
                                 <a href="/" aria-current="page" class="<?= urlId('/')? 'bg-gray-900 text-white': 'text-gray-300 ';?> rounded-md px-3 py-2 text-sm font-medium hover:bg-white/5 hover:text-white'">Home</a>
-                                <a href="/about.php" class="<?=  urlId('/about.php')? 'bg-gray-900 text-white': 'text-gray-300 ';?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
-                                <a href="/contact.php" class="<?= urlId('/contact.php')? 'bg-gray-900 text-white':'text-gray-300';?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
+                                <a href="/about" class="<?=  urlId('/about')? 'bg-gray-900 text-white': 'text-gray-300 ';?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
+                                <a href="/contact" class="<?= urlId('/contact')? 'bg-gray-900 text-white':'text-gray-300';?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
                             </div>
                         </div>
                     </div>
