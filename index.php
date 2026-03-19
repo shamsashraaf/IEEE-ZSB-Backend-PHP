1e87196b3a04a3c2477b52f1f1a4f2123cf1457e
Shams Ashraf <227019490+shamsashraaf@users.noreply.github.com>
2026-03-19 03:10:28 +0200

    Fixing styling using the  superglobal

--- index.php
+++ index.php
@@ -1,5 +1,17 @@
 <?php
 
 $heading = "Home";
+function dd($value)
+{
+    echo "<pre>";
+    var_dump($value);
+    echo "</pre>";
+
+    die();
+}
+
+function urlId ($value){
+    return $_SERVER['REQUEST_URI'] === $value;
+}
 
 require "views/index.view.php";
