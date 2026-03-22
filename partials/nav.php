8c0d861f1e1ed903844c5429da3ea4a221072a2f
Shams Ashraf <227019490+shamsashraaf@users.noreply.github.com>
2026-03-22 02:30:30 +0200

    Render Notes and Note Pages

--- partials/nav.php
+++ partials/nav.php
@@ -10,6 +10,7 @@
                                 <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-white/5 hover:text-white" -->
                                 <a href="/" aria-current="page" class="<?= urlId('/')? 'bg-gray-900 text-white': 'text-gray-300 ';?> rounded-md px-3 py-2 text-sm font-medium hover:bg-white/5 hover:text-white'">Home</a>
                                 <a href="/about" class="<?=  urlId('/about')? 'bg-gray-900 text-white': 'text-gray-300 ';?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">About</a>
+                                <a href="/notes" class="<?=  urlId('/notes')? 'bg-gray-900 text-white': 'text-gray-300 ';?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Notes</a>
                                 <a href="/contact" class="<?= urlId('/contact')? 'bg-gray-900 text-white':'text-gray-300';?> rounded-md px-3 py-2 text-sm font-medium text-gray-300 hover:bg-white/5 hover:text-white">Contact</a>
                             </div>
                         </div>
