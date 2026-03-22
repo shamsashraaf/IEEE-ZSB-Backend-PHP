dea0fd1e772b18b01e81d148c77d5eb58161560f
Shams Ashraf <227019490+shamsashraaf@users.noreply.github.com>
2026-03-22 19:56:26 +0200

    Introduction to Authorization

--- views/note.view.php
+++ views/note.view.php
@@ -6,7 +6,7 @@
 <main>
     <div class="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
 
-        <p class="mb-">
+        <p class="mb-5">
             <a href="/notes" class="text-blue-500 hover:underline">go back...</a>
         </p>
 
